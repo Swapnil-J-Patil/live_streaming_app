48179ecf16a44d4665c94a06e181e2f400486ac7
Swapnil <swapnilp0501@gmail.com>
2026-01-20 12:14:45 +0530

    feat: Integrated firebase and added signup screen.

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:live_streaming_app/views/signup.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Live Streaming App',
+      theme: ThemeData(
+        fontFamily: GoogleFonts.ibmPlexSans().fontFamily ,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+      ),
+      home: const Signup(),
+    );
+  }
+}
+
---
+++ lib/views/signup.dart
@@ -0,0 +1,96 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:form_validator/form_validator.dart';
+
+class Signup extends StatefulWidget {
+  const Signup({super.key});
+
+  @override
+  State<Signup> createState() => _SignupState();
+}
+
+class _SignupState extends State<Signup> {
+
+  String? username;
+  String? email;
+  String? password;
+  GlobalKey<FormState> key = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Sign Up')),
+      body: Form(
+        key: key,
+        child: ListView(
+          padding: const EdgeInsets.all(12.0),
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Username",
+              ),
+              validator: ValidationBuilder().maxLength(10).build(),
+              onChanged: (value) {
+                username = value;
+              },
+            ),
+            const SizedBox(height: 12.0),
+            TextFormField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Email",
+              ),
+              validator: ValidationBuilder().email().maxLength(50).build(),
+              onChanged: (value) {
+                email = value;
+              },
+            ),
+            const SizedBox(height: 12.0),
+            TextFormField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Password",),
+              validator: ValidationBuilder().minLength(6).maxLength(15).build(),
+              onChanged: (value) {
+                password = value;
+              },
+            ),
+            const SizedBox(height: 12.0),
+
+            ElevatedButton(
+              onPressed: () async {
+                if (key.currentState?.validate() ?? false) {
+                  try {
+                    await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                      email: email!,
+                      password: password!,
+                    );
+                  } on FirebaseAuthException catch (e) {
+                    if (!mounted) return;
+
+                    if (e.code == 'weak-password') {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text("The password provided is too weak.")),
+                      );
+                    } else if (e.code == 'email-already-in-use') {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text("The account already exists for that email.")),
+                      );
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                         SnackBar(content: Text(e.message ?? "Signup failed")),
+                      );
+                    }
+                  }
+                }
+              },
+              child: const Text("Sign Up"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
