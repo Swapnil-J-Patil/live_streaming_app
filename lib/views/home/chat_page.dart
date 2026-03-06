6d9c47f26271fd894aa7e8bea826b765fe705e0d
Swapnil <swapnilp0501@gmail.com>
2026-03-06 11:19:11 +0530

    feat: Added chat page

--- lib/views/home/chat_page.dart
+++ lib/views/home/chat_page.dart
@@ -1,9 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatefulWidget {
 
   final DocumentSnapshot doc;
+
   const ChatPage({super.key, required this.doc});
 
   @override
@@ -11,10 +13,53 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  TextEditingController message = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Chat Page")),
+      body: Column(
+          children: [
+            Expanded(child: StreamBuilder(stream: widget.doc.reference.collection('messages').snapshots(), builder: (context, snapshot)
+            {
+              if(snapshot.hasData)
+                {
+                  if(snapshot.data?.docs.isEmpty ?? true)
+                    {
+                      return Text("No Messages");
+                    }
+                  return Text("abdfjksld");
+                }
+              else
+                {
+                  return CircularProgressIndicator();
+                }
+            })),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Expanded(child: TextFormField(
+                    decoration: InputDecoration(labelText: "Your Message",
+                    ),
+                    controller: message,),
+                  ),
+
+                  ElevatedButton(onPressed: () async {
+                    widget.doc.reference.collection('messages').add({
+                      'time' : DateTime.now(),
+                      'uid' : FirebaseAuth.instance.currentUser!.uid,
+                      'message' : message.text
+
+                      //28:14
+                    });
+                  }, child: Text("Send"),)
+                ],
+              ),
+            )
+          ]
+      ),
     );
   }
 }
